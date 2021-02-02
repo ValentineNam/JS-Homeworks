9e9fb43ae3c7417139ca1c7b3d818d8c1602011b
ValentineNam <v4lentine.nam@gmail.com>
2021-02-02 23:15:02 +0300

    add some function

--- Eloquent_javascript/HomeWork2.js
+++ Eloquent_javascript/HomeWork2.js
@@ -40,14 +40,24 @@ function every(arr, test) {
 }
 
 function some(arr, test) {
-    
+    function testing(arr) {
+        let flag = false;
+        arr.forEach(element => {
+            if (test(element)) {
+                flag = true;
+                return;
+            }
+        });
+        return flag;
+    }
+    return testing(arr);
 }
 
 console.log(every([NaN, NaN, NaN], isNaN));
 // → true
 console.log(every([NaN, NaN, 4], isNaN));
 // → false
-// console.log(some([NaN, 3, 4], isNaN));
-// // → true
-// console.log(some([2, 3, 4], isNaN));
-// // → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
